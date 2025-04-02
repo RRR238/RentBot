56370290203f136ca4204924c64d26498a67ff17
Richard Macus <richard.macus.external@constellium.com>
2025-04-02 16:20:37 +0200

    DB interactions added to Nehnutelnosti_sk

--- Shared/Geolocation.py
+++ Shared/Geolocation.py
@@ -4,8 +4,10 @@ def get_coordinates(place_name):
     """Returns the latitude and longitude of a given place."""
     geolocator = Nominatim(user_agent="geo_finder")
     location = geolocator.geocode(place_name)
+    if not location:
+        location = geolocator.geocode(place_name.replace('ulica',''))
 
     if location:
         return location.latitude, location.longitude
     else:
-        return None  # Return None if the place is not found
+        return None
