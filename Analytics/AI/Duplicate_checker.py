adf26f2599893ab67d55c29ea3ebdb085ccbffc8
RRR238 <richardmacus0@gmail.com>
2025-05-01 13:18:55 +0200

    duplicates checking fixed

--- Analytics/AI/Duplicate_checker.py
+++ Analytics/AI/Duplicate_checker.py
@@ -1,11 +1,6 @@
-from Shared.Vector_database.Elasticsearch import Vector_DB
 from Scrapping.Rent_offers_repository import Rent_offers_repository
 from Analytics.config import CONN_STRING
-from Shared.LLM import LLM
 
-
-vdb = Vector_DB('rent-bot-index')
-llm = LLM()
 repository = Rent_offers_repository(CONN_STRING)
 
 
@@ -14,19 +9,17 @@ count = 1
 duplicates = []
 found_dups = 0
 l = len(sources)
-for i in sources:
+for i in sources[:5]:
 
-    print(f"{count}/{l}")
+    print(f"processing: {count}/{l}")
     count += 1
     offer = repository.get_offer_by_id_or_url(i)
     if repository.duplicate_exists(offer.price_rent,
-                                    offer.size+1,
-                                    offer.coordinates) and repository.duplicate_exists(
-                                                            offer.price_rent,
-                                                            offer.size - 1,
-                                                            offer.coordinates):
+                                    offer.size,
+                                    offer.coordinates):
         duplicates.append(i)
         repository.delete_by_source_urls([i])
         found_dups += 1
+        print(f"dups found: {count}")
 
 print(found_dups)
