347c58d43f9dcdfdeded2cd1e12d67c42afd4afd
RRR238 <richardmacus0@gmail.com>
2025-05-01 10:03:29 +0200

    duplicate checker updated

--- Analytics/AI/Duplicate_checker.py
+++ Analytics/AI/Duplicate_checker.py
@@ -1,7 +1,6 @@
 from Shared.Elasticsearch import Vector_DB
 from Scrapping.Rent_offers_repository import Rent_offers_repository
 from Analytics.config import CONN_STRING
-import json
 from Shared.LLM import LLM
 
 
@@ -12,43 +11,22 @@ repository = Rent_offers_repository(CONN_STRING)
 
 sources = repository.get_all_source_urls()
 count = 1
-considered = []
 duplicates = []
+found_dups = 0
+l = len(sources)
 for i in sources:
-    if i not in considered:
-        print(count)
-        count += 1
-        element = vdb.get_element(source_url=i)
-        most_similar = vdb.search_similar_documents(element['embedding'],k=2)
-        try:
-            if element['metadata']['price_rent'] == most_similar[1]['_source']['metadata']['price_rent']:
-                if element['metadata']['size']+1 >= most_similar[1]['_source']['metadata']['size'] and element['metadata']['size']-1 <= most_similar[1]['_source']['metadata']['size']:
-                    if element['metadata']['coordinates'] == most_similar[1]['_source']['metadata']['coordinates']:
 
-            #if most_similar[1]['_score'] >= 0.97:
-                        # webbrowser.open(i)
-                        # webbrowser.open_new_tab(most_similar[1]['_source']['metadata']['source_url'])
-                        # is_duplicate = input('is duplicate (1/0) ?')
-                        # duplicates.append({'source':i,
-                        #                    'most_similar':most_similar[1]['_source']['metadata']['source_url'],
-                        #                     'similarity':most_similar[1]['_score'],
-                        #                    'is_duplicate':is_duplicate})
-                        duplicates.append(most_similar[1]['_source']['metadata']['source_url'])
-                        #print(f"original url: {i}, most similar url: {most_similar[1]['_source']['metadata']['source_url']}, similarity: {most_similar[1]['_score']}")
-                        #similarities.append(most_similar[1]['_score'])
-                        considered.append(most_similar[1]['_source']['metadata']['source_url'])
-        except:
-            pass
+    print(f"{count}/{l}")
+    count += 1
+    offer = repository.get_offer_by_id_or_url(i)
+    if repository.duplicate_exists(offer.price_rent,
+                                    offer.size+1,
+                                    offer.coordinates) and repository.duplicate_exists(
+                                                            offer.price_rent,
+                                                            offer.size - 1,
+                                                            offer.coordinates):
+        duplicates.append(i)
+        repository.delete_by_source_urls([i])
+        found_dups += 1
 
-
-print(len(duplicates))
-with open('duplicates.json', 'w',encoding="utf-8") as json_file:
-            json.dump(duplicates, json_file, ensure_ascii=False, indent=4)
-
-# with open('duplicates.json', 'r',encoding="utf-8") as json_file:
-#             duplicates = json.load(json_file)
-# #repository.delete_by_source_urls(duplicates)
-# for i in duplicates:
-#     vdb.delete_element(source_url=i)
-
-
+print(found_dups)
