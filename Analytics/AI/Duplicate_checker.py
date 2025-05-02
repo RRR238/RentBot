eadaebff721f7624b24f7bffee8e106862430bc1
RRR238 <richardmacus0@gmail.com>
2025-05-02 11:43:52 +0200

    duplicates deleted

--- Analytics/AI/Duplicate_checker.py
+++ Analytics/AI/Duplicate_checker.py
@@ -8,19 +8,19 @@ sources = repository.get_all_source_urls()
 count = 1
 found_dups = 0
 l = len(sources)
-for i in sources[:5]:
+for i in sources:
 
     print(f"processing: {count}/{l}")
+    print(f"dups found: {found_dups}")
     count += 1
     offer = repository.get_offer_by_id_or_url(i)
-    if repository.duplicate_exists(offer.price_rent,
-                                    offer.size,
-                                    offer.coordinates):
-        print(offer.price_rent,
-        offer.size,
-        offer.coordinates)
-        #repository.delete_by_source_urls([i])
+    if repository.duplicate_exists(price=offer.price_rent,
+                                    size=offer.size,
+                                    coordinates=offer.coordinates,
+                                    url=i):
+
+        repository.delete_by_source_urls([i])
         found_dups += 1
-        print(f"dups found: {count}")
+        #print(offer.price_rent,offer.size,offer.coordinates)
 
 print(found_dups)
